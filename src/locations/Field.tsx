af723a61b075344b0ece006d1a76eb2186830652
Jesse Day <jesse.day@adaptagency.com>
2024-03-22 14:03:07 -0400

    Use autoresizer

--- src/locations/Field.tsx
+++ src/locations/Field.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect } from "react";
 import { Stack } from "@contentful/f36-components";
 import { FieldAppSDK } from "@contentful/app-sdk";
-import { /* useCMA, */ useSDK } from "@contentful/react-apps-toolkit";
+import { useAutoResizer, useSDK } from "@contentful/react-apps-toolkit";
 import { IconButton } from "@contentful/f36-components";
 import FullWidth from "../components/full-width";
 import ImageLeft from "../components/image-left";
 import ImageRight from "../components/image-right";
 
 const Field = () => {
+  useAutoResizer();
+
   const sdk = useSDK<FieldAppSDK>();
   const options = sdk.field.validations[0].in;
   const types:Record<string, {icon: JSX.Element, label: string}> = {
@@ -32,14 +34,6 @@ const Field = () => {
     sdk.field.setValue(value);
   }, [value, sdk.field]);
 
-  /*
-     To use the cma, inject it as follows.
-     If it is not needed, you can remove the next line.
-  */
-  // const cma = useCMA();
-  // If you only want to extend Contentful's default editing experience
-  // reuse Contentful's editor components
-  // -> https://www.contentful.com/developers/docs/extensibility/field-editors/
   return (
     <Stack
       flexDirection="column"
